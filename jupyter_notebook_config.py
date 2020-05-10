73130cf5a4d2689e3ecb070aabba4a75d546e186
skark <s@example.com>
2020-05-10 09:20:09 -0700

    docker

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -8,6 +8,9 @@ import errno
 import stat
 
 c = get_config()
+c.Session.debug = True
+
+"""
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
@@ -53,3 +56,4 @@ distinguished_name = req_distinguished_name
 # the environment
 if 'NB_UMASK' in os.environ:
     os.umask(int(os.environ['NB_UMASK'], 8))
+"""
